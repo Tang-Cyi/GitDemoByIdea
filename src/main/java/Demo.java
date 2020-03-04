52fd48ac86906d33ed68a766c7b5c226b5b29758
tcy <772736535@qq.com>
2020-03-04 21:58:40 +0800

    Idea的Git测试

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,10 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("你好，git");
+        System.out.println("你好1，git");
+        System.out.println("你好2，git");
+        System.out.println("你好3，git");
+        System.out.println("你好4，git");
+        System.out.println("你好5，git");
     }
 }
